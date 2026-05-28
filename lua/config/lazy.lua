5f3de964427df6a7876ac655a762423ec893aa87
cayasde <mydev.nice@gmail.com>
2026-05-28 19:19:22 -0400

    feat(ui): add smooth animated scrolling

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -101,6 +101,20 @@ require("lazy").setup({
     },
   },
   {
+    "karb94/neoscroll.nvim",
+    opts = {
+      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      hide_cursor = true,
+      stop_eof = true,
+      respect_scrolloff = false,
+      cursor_scrolls_alone = true,
+      duration_multiplier = 1.0,
+      easing = "linear",
+      performance_mode = false,
+      ignored_events = { "WinScrolled", "CursorMoved" },
+    },
+  },
+  {
     "saghen/blink.cmp",
     version = "1.*",
     opts = {
