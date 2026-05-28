75e4cf3c5ebd067be1395fbcb721b85197019575
cayasde <mydev.nice@gmail.com>
2026-05-28 19:35:01 -0400

    feat(ui): add noice message and cmdline ui

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -115,6 +115,63 @@ require("lazy").setup({
     },
   },
   {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      max_width = 80,
+      top_down = false,
+      render = "compact",
+    },
+    config = function(_, opts)
+      local notify = require("notify")
+      notify.setup(opts)
+      vim.notify = notify
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = true,
+      },
+      messages = {
+        enabled = true,
+      },
+      notify = {
+        enabled = true,
+        view = "notify",
+      },
+      popupmenu = {
+        enabled = true,
+        backend = "nui",
+      },
+      cmdline = {
+        enabled = true,
+        view = "cmdline_popup",
+      },
+      views = {
+        mini = {
+          timeout = 2500,
+        },
+      },
+    },
+  },
+  {
     "saghen/blink.cmp",
     version = "1.*",
     opts = {
