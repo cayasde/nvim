ff11549f0ed022a9650d39686128d53f11b0d143
cayasde <mydev.nice@gmail.com>
2026-05-28 22:31:21 -0400

    feat(nvim): add colorful window separator plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,6 +37,28 @@ require("lazy").setup({
     end,
   },
   {
+    "nvim-zh/colorful-winsep.nvim",
+    event = { "WinLeave" },
+    config = function()
+      require("colorful-winsep").setup({
+        border = "bold",
+        excluded_ft = { "packer", "TelescopePrompt", "mason", "lazy", "oil" },
+        highlight = "#79c0ff",
+        animate = {
+          enabled = "progressive",
+          progressive = {
+            delay = 16,
+            vertical_lerp_factor = 0.15,
+            horizontal_lerp_factor = 0.15,
+          },
+        },
+        indicator_for_2wins = {
+          position = false,
+        },
+      })
+    end,
+  },
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
