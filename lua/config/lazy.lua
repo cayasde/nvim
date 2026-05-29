def94cfb9c226ec86517b73be6c51c73de750b28
cayasde <mydev.nice@gmail.com>
2026-05-28 22:42:02 -0400

    feat(nvim): add persistent inline diagnostics

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -59,6 +59,42 @@ require("lazy").setup({
     end,
   },
   {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup({
+        preset = "modern",
+        transparent_bg = true,
+        transparent_cursorline = true,
+        options = {
+          show_source = {
+            enabled = false,
+          },
+          show_code = true,
+          throttle = 20,
+          softwrap = 30,
+          multilines = {
+            enabled = true,
+            always_show = true,
+          },
+          show_all_diags_on_cursorline = false,
+          show_diags_only_under_cursor = false,
+          enable_on_insert = false,
+          overflow = {
+            mode = "wrap",
+            padding = 0,
+          },
+          override_open_float = true,
+        },
+      })
+
+      vim.diagnostic.config({
+        virtual_text = false,
+      })
+    end,
+  },
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
